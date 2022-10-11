9bdb3bb07d973256470589dbc6e48cf0ab3259cc
alifhs <alif.hs@outlook.com>
2022-10-12 01:40:03 +0600

    add nav bar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 import "./index.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logos from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Sidebar = () => {
   return (
     <div className="nav-bar">
@@ -9,6 +11,17 @@ const Sidebar = () => {
         <img src={Logos} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="logo_sub" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeClassName="active" to="/">
+            <FontAwesomeIcon  icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeClassName="active" className="about-link" to="/about">
+            <FontAwesomeIcon  icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeClassName="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon  icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
     </div>
   );
 };
